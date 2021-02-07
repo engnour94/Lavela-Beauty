964ad3b9043115f2145703cbe50ce56a967e6cad
engnour94 <nour.abuelenein@hotmail.com>
2021-02-07 17:04:05 +0200

    creat jss

---
+++ app.js
@@ -0,0 +1,19 @@
+
+console.log("test")
+alert('Welcome To Lavella Beauty Shop');
+console.log("hello");
+var naming = prompt("enter your name");
+
+alert('Hi  '+naming);
+var buy = confirm("Do you want to buy?","Default");
+
+if(buy==true){
+    alert("she is a buyer");
+}
+else{
+    alert("she i a vieitor");
+}
+
+
+
+
